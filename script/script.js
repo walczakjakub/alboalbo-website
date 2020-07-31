dcbb64e9caef2a9c1ea51d9ccb24c5fb9dd4cfb5
walczakjakub <jakub_walczak@tlen.pl>
2020-07-31 13:17:10 +0200

    fix squad info content showing + style

--- script/script.js
+++ script/script.js
@@ -2,18 +2,22 @@
 let squadInfo = document.getElementById("squad-info");
 let squadImgBox = document.getElementsByClassName("squad-img-box");
 
-function showSquadInfo() {
-  squadInfo.style.display = "block";  
-
-  let infoName = document.getElementById("squad-info-name");
-  let infoBio = document.getElementById("squad-info-bio");
-
-  infoName.innerHTML = squadInfoContent[i].name;
-  infoBio.innerHTML = squadInfoContent[i].bio;
-};
-
 for (let i=0; i<5; i++) {
+  let squadNumber = i;  
   squadImgBox[i].addEventListener("click", showSquadInfo);
-};
-  
 
+  function showSquadInfo() {
+    squadInfo.style.display = "block"; 
+    let infoName = document.getElementById("squad-info-name");
+    let infoBio = document.getElementById("squad-info-bio");
+    infoName.innerHTML = squadInfoContent[i].name;
+    infoBio.innerHTML = squadInfoContent[i].bio;
+  };
+
+  // window.addEventListener("click", hideSquadInfo);
+
+  // function  hideSquadInfo() {
+  //   squadInfo.style.display = "none";
+  // };
+
+};
