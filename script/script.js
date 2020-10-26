93ceef8b369fd59faacd600c65a40ad6aa44a82b
walczakjakub <jakub_walczak@tlen.pl>
2020-10-26 16:28:02 +0100

    add show/hide squad info; add back to top button; add footer; add socials links; add button and video animations; add socials links to objects

--- script/script.js
+++ script/script.js
@@ -1,23 +1,52 @@
 // click on squad memeber to see info
 let squadInfo = document.getElementById("squad-info");
+let squadInfoOverlay = document.getElementById("squad-info-overlay");
+let squadInfoClose = document.getElementById("squad-info-close");
 let squadImgBox = document.getElementsByClassName("squad-img-box");
 
-for (let i=0; i<5; i++) {
-  let squadNumber = i;  
-  squadImgBox[i].addEventListener("click", showSquadInfo);
+if (squadInfo.style.display === "") {
+  for (let i=0; i<5; i++) {  
+    let squadNumber = i;  
+    squadImgBox[i].addEventListener("click", showSquadInfo);
 
-  function showSquadInfo() {
-    squadInfo.style.display = "block"; 
-    let infoName = document.getElementById("squad-info-name");
-    let infoBio = document.getElementById("squad-info-bio");
-    infoName.innerHTML = squadInfoContent[i].name;
-    infoBio.innerHTML = squadInfoContent[i].bio;
+    function showSquadInfo() {
+      squadInfo.style.display = "block"; 
+      squadInfoOverlay.style.display = "block"; 
+      let infoName = document.getElementById("squad-info-name");
+      let infoBio = document.getElementById("squad-info-bio");
+      infoName.innerHTML = squadInfoContent[i].name;
+      infoBio.innerHTML = squadInfoContent[i].bio;
+    };
   };
+}; 
 
-  // window.addEventListener("click", hideSquadInfo);
+squadInfoOverlay.addEventListener("click", hideSquadInfo);
+squadInfoClose.addEventListener("click", hideSquadInfo);
 
-  // function  hideSquadInfo() {
-  //   squadInfo.style.display = "none";
-  // };
-
+function  hideSquadInfo() {
+  if (squadInfo.style.display !== "") {
+    squadInfo.style.display = "none"; 
+    squadInfoOverlay.style.display = "none";  
+  };
 };
+
+//BACK TO TOP BUTTON
+const backToStartButton = document.querySelector("#back-to-start-button");
+
+window.addEventListener("scroll", showBtsButton);
+
+function showBtsButton() {
+  if (window.pageYOffset >300) {
+    backToStartButton.style.display = "block";
+  }
+  else {
+    backToStartButton.style.display = "none";
+  };
+};
+
+backToStartButton.addEventListener("click", backToStart);
+
+function backToStart() {
+  window.scrollTo(0, 0);
+};
+
